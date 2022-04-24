d8bbc5c3a6baa00a06b5b55ac6affc5a6a221c09
huangpeilin <huangpeilin@qunjielong.com>
2022-04-24 16:08:33 +0800

    feat: 增加批量同时请求的情况

--- test/cliendSendReq.ts
+++ test/cliendSendReq.ts
@@ -6,16 +6,21 @@ const request = require("request")
 
 
 let url = "http://127.0.0.1:8081/order/add"
-console.log("开始发送请求", url);
 
-// 发出请求
-request({
-    url,
-    method: "POST"
-}, (err: any, response: any, body: any): void => {
-    if(err){
-        console.log("err", err);
-        return
-    }
-    console.log("res", response.statusCode, body)
-})
+
+function test(){
+    // 发出请求
+    console.log("开始发送请求", url);
+    request({
+        url,
+        method: "POST"
+    }, (err: any, response: any, body: any): void => {
+        if(err){
+            console.log("err", err);
+            return
+        }
+        console.log("res", response.statusCode, body)
+    })
+}
+
+new Array(50).fill(5).forEach(() => test())
