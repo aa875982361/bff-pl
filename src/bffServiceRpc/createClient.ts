d7a760fafd1bd813b521a9d4099db507ef410cfc
huangpeilin <huangpeilin@qunjielong.com>
2022-04-25 11:27:31 +0800

    feat: 兼容数据截断问题，修改连接成功后再发送数据

--- src/bffServiceRpc/createClient.ts
+++ src/bffServiceRpc/createClient.ts
@@ -9,7 +9,7 @@ import { handleStickPackage } from "../utils/buffSplit";
 /**
  * 客户端连接缓存
  */
-const clientMap: Record<string, Socket> = {
+const clientMap: Record<string, Promise<Socket>> = {
 }
 /**
  * 事件列表
@@ -26,61 +26,76 @@ let requestId = 1000
  * @param port 
  * @returns 
  */
-function createClient(port: number): Socket{
+async function createClient(port: number): Promise<Socket>{
     // 判断是否有缓存
     // 需要判断是否还在连接
-    const cacheClient = clientMap[port]
-    if(cacheClient && cacheClient.readyState !== "closed"){
-        return clientMap[port]
+    if(clientMap[port]){
+        const cacheClient = await clientMap[port]
+        if(cacheClient.readyState !== "closed"){
+            // 存在缓存
+            return cacheClient
+        }
     }
     console.log("新起一个链接", port);
+    // 设置缓存 等待连接成功
+    clientMap[port] = new Promise<Socket>((resolve, reject) => {
+        const client = createConnection(port)
+        client.on("connect", () => {
+            console.log("client connect", port);
+            resolve(client)
+        })
     
-    const client = createConnection(port)
-    clientMap[port] = client
-    client.on("connect", () => {
-        console.log("client connect", port);
-    })
-    
-    client.on("data", (data) => {
-        // 这里也会有数据粘包问题
-        handleStickPackage(data, (onePacakgeBuff: Buffer) => {
-            let result
-            try {
-                // 反序列化数据
-                result = decodeBuff(onePacakgeBuff)
-            } catch (error) {
-                console.log("解析数据包失败，跳过", error)
-                return
-            }
-            if(result.requestType === RequsetType.RESPONSE){
-                // 返回数据
-                // 找到请求id对应的callback运行
-                const requestId = result.requestId
-                // 查找回调
-                const callback = eventMap[requestId]
-                // 检查是否存在回调
-                if(callback && typeof callback === "function"){
-                    // 触发回调
-                    callback(result.body)
-                    // 运行完就可以解除关系
-                    delete eventMap[requestId]
+        // 上次未处理buffer
+        let preBuffer:Buffer = Buffer.alloc(0)
+        
+        client.on("data", (data) => {
+            // 拼接上次未处理
+            data = Buffer.concat([preBuffer, data])
+            // 这里也会有数据粘包问题
+            preBuffer = handleStickPackage(data, (onePacakgeBuff: Buffer) => {
+                let result
+                try {
+                    // 反序列化数据
+                    result = decodeBuff(onePacakgeBuff)
+                } catch (error) {
+                    console.log("解析数据包失败，跳过", error)
+                    return
                 }
-            }else if(result.requestType === RequsetType.REQUEST){
-                // 服务端向客户端发送请求
-                console.log("服务端发送的请求");
-                
-            }else{
-                console.error("非法请求类型")
-            }
+                if(result.requestType === RequsetType.RESPONSE){
+                    // 返回数据
+                    // 找到请求id对应的callback运行
+                    const requestId = result.requestId
+                    // 查找回调
+                    const callback = eventMap[requestId]
+                    // 检查是否存在回调
+                    if(callback && typeof callback === "function"){
+                        // 触发回调
+                        callback(result.body)
+                        // 运行完就可以解除关系
+                        delete eventMap[requestId]
+                    }
+                }else if(result.requestType === RequsetType.REQUEST){
+                    // 服务端向客户端发送请求
+                    console.log("服务端发送的请求");
+                    
+                }else{
+                    console.error("非法请求类型")
+                }
+            })
+        })
+
+        client.on("error", (err) => {
+            console.log("client connect error", err);
+            reject(err)
+        })
+    
+        client.on("close", () => {
+            console.log("client closed", port);
+            delete clientMap[port]
         })
     })
-
-    client.on("close", () => {
-        console.log("client closed", port);
-        delete clientMap[port]
-    })
-    // 定时发送心跳保活
-    return client
+    return clientMap[port]
+    
 }
 
 /**
@@ -89,9 +104,9 @@ function createClient(port: number): Socket{
  * @param buf 
  * @param callback 
  */
-export function sendDataToService(port: number, body: RequestBody, callback: (reponseBody: ResponseBody) => any){
+export async function sendDataToService(port: number, body: RequestBody): Promise<ResponseBody>{
     // 创建一个客户端连接，或者用存在的连接
-    const client = createClient(port)
+    const client = await createClient(port)
     // 获取当前请求id，并自增请求id, 保证不重复
     const currentRequestId = requestId++
     // 编码数据
@@ -101,9 +116,39 @@ export function sendDataToService(port: number, body: RequestBody, callback: (re
         body,
         timeOut: 60
     })
-    // 绑定回调到当前的请求id
-    eventMap[currentRequestId] = callback
     // console.log("调用客户端连接发送数据", port, currentRequestId);
-    // 发送数据
-    client.write(sendBuff)
+    try {
+        // 发送数据
+        return _clientSendData(client, sendBuff, currentRequestId)
+    } catch (error) {
+        console.log("发送数据包失败", currentRequestId);
+        // 重试
+        return sleep(1000).then(() => sendDataToService(port, body))
+    }
+}
+
+/**
+ * 真正发送数据到服务端
+ * @param client 客户端链接
+ * @param data 数据
+ * @param requestId 请求id
+ * @returns 
+ */
+function _clientSendData(client: Socket, data: Buffer, requestId: number): Promise<ResponseBody> {
+    return new Promise((resolve, reject) => {
+        // 绑定回调到当前的请求id
+        eventMap[requestId] = resolve
+        try {
+            client.write(data)
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
+
+function sleep(sleepTime: number): Promise<void> {
+    return new Promise((resolve) => {
+        setTimeout(resolve, sleepTime)
+    })
 }
