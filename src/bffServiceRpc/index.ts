b210f5682bac1f39e9f3d31bca7458b5493105c3
huangpeilin <huangpeilin@qunjielong.com>
2022-04-25 10:29:09 +0800

    feat: 修改bff的底层框架

--- src/bffServiceRpc/index.ts
+++ src/bffServiceRpc/index.ts
@@ -4,70 +4,64 @@
 
 import { ResponseBody } from "../utils/buffHandler";
 import { sendDataToService } from "./createClient";
-import { createServer, IncomingMessage, ServerResponse } from "http"
- const BFF = createServer((req, res) => {
-     console.log("监听服务");
-     
-     handleBFF(req, res);
- });
- 
- let port = 8081
- BFF.listen(port, () => {
-     console.log(`BFF Server is running at ${port} port`);
- });
- 
- function handleBFF(req: IncomingMessage, res: ServerResponse): void {
-     console.log("调用bff层接口", req.url);
-     switch (req.url) {
-         case '/order/add':
-             addOrder(req, res);
-             break;
-         default:
-             res.end('{ code: 500, msg: "route not found", data: "" }');
-             break;
-     }
- }
- 
- // 处理添加订单方法
- function addOrder(req: IncomingMessage, res: ServerResponse): void {
-     if (req.method !== 'POST') {
-         res.end('{ code: 500, msg: "route method not support", data: "" }');
-         return;
-     }
- 
-     let data = '';
-     req.on('data', (chunk: Buffer) => {
-         data += chunk;
-     });
- 
-     req.on('end', async () => {
-         const { code: orderCode, data: orderResult} = await publicRequest(
-             4444,
-             "addOrder",
-             data
-         );
-         console.log("orderResult", orderResult, orderCode);
-         
-         const {code: dataCode, data: dataResult} = await publicRequest(
-             4445,
-             "getData",
-             data
-         );
-         console.log("dataResult", dataResult, dataCode)
-         res.end(JSON.stringify({
-             code: 200,
-             data: { orderResult, dataResult }
-         }));
-     });
- }
+import express from "express"
+import bodyParser from "body-parser";
+
+const port = "8081"
+var app = express();
+// 唯一请求id
+let requestBffId = 1000
+
+// 解析body的数据
+app.use(bodyParser.json())
+
+// 当对主页发出 GET 请求时
+app.post('/order/add', async function(req, res) {
+    // console.log("res.body", req.body);
+    const { testIndex = 0 } = req.body || {}
+    if(testIndex){
+        console.log("testIndex", testIndex);
+    }
+    const currentRequestBffId = requestBffId++
+    let data = {
+        msg: "发送给基础服务的数据"
+    }
+    // console.log("开始处理 currentRequestBffId ", currentRequestBffId);
+    
+    // 处理订单
+    const { code: orderCode, data: orderResult} = await publicRequest(
+        4444,
+        "addOrder",
+        data,
+    );
+    // console.log("orderResult", orderResult, orderCode);
+    
+    const {code: dataCode, data: dataResult} = await publicRequest(
+        4445,
+        "getData",
+        data
+    );
+    // console.log("dataResult", dataResult, dataCode)
+    // 处理完成
+    console.log("处理 currentRequestBffId 完成 ", currentRequestBffId);
+    res.end(JSON.stringify({
+        code: 200,
+        data: { orderResult, dataResult }
+    }));
+});
+
+app.listen(port, () => {
+    console.log("开始监听", port);
+})
+
  
  // 公共请求方法
  async function publicRequest(port: number, method: string, data: any): Promise<ResponseBody> {
      // 处理port
-     console.log("开始处理", port, data);
+    //  console.log("开始处理", port, data);
      return new Promise((resolve) => {
          function callback(res: ResponseBody){
-             console.log("处理完成", port, data);
+            //  console.log("处理完成", port, data);
             resolve(res)
          }
          // 发送数据
