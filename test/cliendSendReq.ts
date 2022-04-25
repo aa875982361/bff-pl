47685d2265c145be6bb8f15f9be99fdaade74f02
huangpeilin <huangpeilin@qunjielong.com>
2022-04-25 20:14:04 +0800

    feat: 增加curl 调用请求

--- test/cliendSendReq.ts
+++ test/cliendSendReq.ts
@@ -3,12 +3,15 @@
  */
 
 const request = require("request")
+const childProcess = require("child_process")
 
 
 let url = "http://127.0.0.1:8081/order/add"
-let maxNum = 1000
+let maxNum = 2500
 let successNum = 0
 let failNum = 0
+let successList: number[] = []
+let failList: number[] = []
 
 function test(index: number){
     // 发出请求
@@ -25,24 +28,99 @@ function test(index: number){
             if(err){
                 console.log("err", err);
                 failNum++
+                failList.push(index)
                 resolve("")
                 return
             }
             if(response.statusCode === 200){
                 // console.log(index + "res 200", body)
                 successNum++
+                successList.push(index)
                 resolve(response.statusCode)
             }else{
-                console.log(index + " res not 200", response.statusCode, body)
+                // console.log(index + " res not 200", response.statusCode, body)
                 failNum++
+                failList.push(index)
                 resolve("")
             }
         })
     })
 }
 
-Promise.all(new Array(maxNum).fill(1).map((num, index) => test(index))).then(() => {
-    console.log("successNum", successNum);
-    console.log("failNum", failNum);
-    console.log("all", maxNum);
+childProcess.error = (err: any) => {
+    console.log("childProcess error", err);
+}
+
+process.on("error", (err: any) => {
+    console.log("process error", err);
 })
+
+/**
+ * 测试通过curl
+ * @param index 
+ * @returns 
+ */
+function testByCurl(index: number){
+    return new Promise((resolve, reject) => {
+        // const commandStr = `curl -X POST ${url}`
+        const commandStr = `curl -H "Content-Type: application/json" -X POST -d '${JSON.stringify({
+            testIndex: index,
+        })}'  ${url}`
+        try {
+            const curlCommand = childProcess.exec(commandStr, {maxBuffer: 1024*1024*100}, (err: string, stdout: string, stderr: string) => {
+                if(err){
+                    debugger
+                    console.log("执行curl失败", commandStr, err);
+                    failNum++
+                    failList.push(index)
+                // }else if(stderr){
+                //     // console.log("执行curl 产生失败日志", commandStr, stderr);
+                //     failNum++
+                //     failList.push(index)
+                }else{
+                    successNum++
+                    successList.push(index)
+                    // console.log("执行curl成功");
+                }
+                resolve("")
+            })
+            curlCommand.on("error", (err: any) => {
+                console.log("curlCommand error", err);
+                
+                resolve("")
+            })
+            curlCommand.on("close", (err: any) => {
+                console.log("curlCommand close", err);
+                
+                resolve("")
+            })
+        } catch (error) {
+            console.log("exec 执行失败", error);
+            failNum++
+            failList.push(index)
+            resolve("")
+        }
+
+    })
+}
+/**
+ * 主要测试代码
+ */
+function mainTest(): void{
+    successList = []
+    failList = []
+    const preTime = +new Date()
+    Promise.all(new Array(maxNum).fill(1).map((num, index) => testByCurl(index))).then(() => {
+        console.log("successList", successList);
+        console.log("failList", failList);
+        
+        console.log("successNum", successNum);
+        console.log("failNum", failNum);
+        console.log("all", maxNum);
+        const currentTime = +new Date()
+        console.log("time", currentTime - preTime);
+
+        // mainTest()
+    })
+}
+mainTest()
