e12474ac2633ddc94d868cf49c6ef14722b2d054
huangpeilin <huangpeilin@qunjielong.com>
2022-04-25 20:12:53 +0800

    feat: 增加一个测试api

--- src/bffServiceRpc/index.ts
+++ src/bffServiceRpc/index.ts
@@ -7,6 +7,7 @@ import { sendDataToService } from "./createClient";
 import express from "express"
 import bodyParser from "body-parser";
 
+
 const port = "8081"
 var app = express();
 // 唯一请求id
@@ -15,6 +16,16 @@ let requestBffId = 1000
 // 解析body的数据
 app.use(bodyParser.json())
 
+app.post('/order/add22', async function(req, res){
+    const currentRequestBffId = requestBffId++
+    console.log("开始处理 currentRequestBffId ", currentRequestBffId);
+    res.end(JSON.stringify({
+        code: 200,
+        data: { 
+            msg: "null data"
+        }
+    }));
+})
 // 当对主页发出 GET 请求时
 app.post('/order/add', async function(req, res) {
     // console.log("res.body", req.body);
@@ -44,7 +55,7 @@ app.post('/order/add', async function(req, res) {
     // console.log("dataResult", dataResult, dataCode)
     // 处理完成
     console.log("处理 currentRequestBffId 完成 ", currentRequestBffId);
-    res.end(JSON.stringify({
+    res.status(200).end(JSON.stringify({
         code: 200,
         data: { orderResult, dataResult }
     }));
