d9562791f1da5009d5a62350d87f42d15e4bc28b
huangpeilin <huangpeilin@qunjielong.com>
2022-04-25 14:08:12 +0800

    feat: 增加批量发送请求脚本

--- test/cliendSendReq.ts
+++ test/cliendSendReq.ts
@@ -6,21 +6,43 @@ const request = require("request")
 
 
 let url = "http://127.0.0.1:8081/order/add"
+let maxNum = 1000
+let successNum = 0
+let failNum = 0
 
-
-function test(){
+function test(index: number){
     // 发出请求
-    console.log("开始发送请求", url);
-    request({
-        url,
-        method: "POST"
-    }, (err: any, response: any, body: any): void => {
-        if(err){
-            console.log("err", err);
-            return
-        }
-        console.log("res", response.statusCode, body)
+    // console.log("开始发送请求", url);
+    return new Promise((resolve, reject) => {
+        request({
+            url,
+            method: "POST",
+            json: true,
+            body: {
+                testIndex: index
+            }
+        }, (err: any, response: any, body: any): void => {
+            if(err){
+                console.log("err", err);
+                failNum++
+                resolve("")
+                return
+            }
+            if(response.statusCode === 200){
+                // console.log(index + "res 200", body)
+                successNum++
+                resolve(response.statusCode)
+            }else{
+                console.log(index + " res not 200", response.statusCode, body)
+                failNum++
+                resolve("")
+            }
+        })
     })
 }
 
-new Array(50).fill(5).forEach(() => test())
+Promise.all(new Array(maxNum).fill(1).map((num, index) => test(index))).then(() => {
+    console.log("successNum", successNum);
+    console.log("failNum", failNum);
+    console.log("all", maxNum);
+})
