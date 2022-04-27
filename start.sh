23ca22c35f1f18e11b88cfffcc02dedc71d3fe14
huangpeilin <huangpeilin@qunjielong.com>
2022-04-27 14:21:04 +0800

    feat: 增加启动测试命令

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 npx tsc
 NAME=${1:-bff-pl};
 BASE_SERVICE_NAME=$NAME-base-service
+BFF_SERVICE_TEST_NAME=$NAME-test
+
 npx pm2 describe $NAME > /dev/null
 RUNNING=$?
 if [ "${RUNNING}" -ne 0 ]; then
@@ -19,4 +21,14 @@ if [ "${RUNNING}" -ne 0 ]; then
 else
     echo "restart app '$BASE_SERVICE_NAME'..."
     npx pm2 restart $BASE_SERVICE_NAME
+fi;
+
+npx pm2 describe $BFF_SERVICE_TEST_NAME > /dev/null
+RUNNING=$?
+if [ "${RUNNING}" -ne 0 ]; then
+    echo "start app '$BFF_SERVICE_TEST_NAME'..."
+    npx pm2 start ./dist/test/cliendSendReq.js --name $BFF_SERVICE_TEST_NAME
+else
+    echo "restart app '$BFF_SERVICE_TEST_NAME'..."
+    npx pm2 restart $BFF_SERVICE_TEST_NAME
 fi;
--- stop.sh
+++ stop.sh
@@ -1,6 +1,7 @@
 tsc
 NAME=${1:-bff-pl};
 BASE_SERVICE_NAME=$NAME-base-service;
+BFF_SERVICE_TEST_NAME=$NAME-test
 
 npx pm2 describe $NAME > /dev/null
 RUNNING=$?
@@ -18,4 +19,13 @@ if [ "${RUNNING}" -ne 0 ]; then
 else
     echo "stop app '$BASE_SERVICE_NAME'..."
     npx pm2 stop $BASE_SERVICE_NAME
+fi;
+
+npx pm2 describe $BFF_SERVICE_TEST_NAME > /dev/null
+RUNNING=$?
+if [ "${RUNNING}" -ne 0 ]; then
+    echo "not app '$BFF_SERVICE_TEST_NAME'..."
+else
+    echo "stop app '$BFF_SERVICE_TEST_NAME'..."
+    npx pm2 stop $BFF_SERVICE_TEST_NAME
 fi;
